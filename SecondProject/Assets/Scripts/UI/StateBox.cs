6776a558ad53b49ff4e87a7548c05571afa0162a
dudjun <dud4817@naver.com>
2022-06-19 17:27:00 +0900

    feat : 플레이어의 상태창 추가
    - 플레이어의 HP, MP, Level을 확인할 수 있는 상태창을 왼쪽 위에 추가

---
+++ SecondProject/Assets/Scripts/UI/StateBox.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StateBox : MonoBehaviour
+{
+    private PlayerStat stat;
+    [SerializeField] private Slider HpSlider;
+    [SerializeField] private Slider MpSlider;
+    [SerializeField] private Text LvText;
+    void Start()
+    {
+        stat = Managers.Game.GetPlayer().GetComponent<PlayerStat>();
+    }
+
+    void Update()
+    {
+        HpSlider.value = stat.Hp / (float)stat.MaxHp;
+        MpSlider.value = stat.Mp / (float)stat.MaxMp;
+        LvText.text = "LV. " + stat.Level;
+    }
+}
