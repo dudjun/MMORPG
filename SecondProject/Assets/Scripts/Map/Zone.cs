60c284ad372080e37f250caedf031aa31698d7dd
dudjun <dud4817@naver.com>
2022-06-21 23:18:57 +0900

    feat : 구역을 알려주는 ui
    특정 지역에 들어가면 장소의 이름을 알려주는 ui가 3초 간 보여진다

---
+++ SecondProject/Assets/Scripts/Map/Zone.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Zone : MonoBehaviour
+{
+    [SerializeField]
+    private string _zoneName;
+    [SerializeField]
+    private GameObject _ui;
+    [SerializeField]
+    private Text _txt;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _txt.text = _zoneName;
+            _ui.SetActive(true);
+            StartCoroutine(WaitAndInactive());
+        }
+    }
+
+    IEnumerator WaitAndInactive()
+    {
+        yield return new WaitForSeconds(3f);
+        _ui.SetActive(false);
+    }
+}
