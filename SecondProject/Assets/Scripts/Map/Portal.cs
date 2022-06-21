1547dc03f733de940a73257c1f3a424efe254f89
dudjun <dud4817@naver.com>
2022-06-22 01:29:20 +0900

    feat : 플레이어만 이동 가능한 포탈 추가

---
+++ SecondProject/Assets/Scripts/Map/Portal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _otherPortal;
+
+    public bool isFirstPortal = true;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && isFirstPortal)
+        {
+            _otherPortal.GetComponent<Portal>().isFirstPortal = false;
+            other.transform.position = _otherPortal.transform.position;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        isFirstPortal = true;
+    }
+}
