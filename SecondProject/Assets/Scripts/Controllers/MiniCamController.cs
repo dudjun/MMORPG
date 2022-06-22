60802d7232332c19ccd45d71247515777e499f2f
dudjun <dud4817@naver.com>
2022-06-22 22:28:12 +0900

    feat : 미니맵 추가
    - 화살표는 플레이어의 위치와 방향을 나타낸다
    - 파란색은 NPC의 위치를 나타낸다
    - 빨간색은 적의 위치를 나타낸다

---
+++ SecondProject/Assets/Scripts/Controllers/MiniCamController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniCamController : MonoBehaviour
+{
+    private GameObject player;
+
+    private void Start()
+    {
+        player = Managers.Game.GetPlayer();
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+    }
+}
